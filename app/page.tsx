aadc373bfe1ee83f913ce5be9bc2a79a86cd3234
crypblizz <tom.terado@nillion.com>
2025-11-13 17:32:54 +0100

    chore: test

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,12 @@ export default function Home() {
         height={90}
         style={{ border: 'none' }}
       />
+      {/* <iframe
+        src="https://nilcc.nillion.com/api/badge?verificationUrl=https%3A%2F%2Fgithub.com%2FNillionNetwork%2Fblind-module-examples%2Fblob%2Fmain%2Fnilcc%2Fattestation-verification%2Fmeasurement-hash.json&reportUrl=https%3A%2F%2F74850b5b-33e2-493b-982e-43aaad9df1bb.workloads.nilcc.sandbox.nillion.network%2Fnilcc%2Fapi%2Fv2%2Freport"
+        width={260}
+        height={90}
+        style={{ border: 'none' }}
+      />
 
       <p>failed demo</p>
       <iframe
@@ -19,7 +25,7 @@ export default function Home() {
         height={90}
         scrolling="no"
         style={{ border: 'none' }}
-      />
+      /> */}
     </main>
   );
 }
